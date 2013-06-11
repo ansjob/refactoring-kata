fd05c1e7f495035d8bf24df3a0c03704a49b7056
Andreas Sjöberg <andreas@tjugohundratalet.se>
2013-06-11 23:53:43 +0200

    added arithmetic compiler sanity checks

---
+++ src/test/java/compiler/sanitychecks/SimpleArithmeticCompilerTest.java
@@ -0,0 +1,39 @@
+package compiler.sanitychecks;
+
+import org.junit.Before;
+
+import semant.amsyntax.Add;
+import semant.amsyntax.Inst;
+import semant.amsyntax.Push;
+import semant.amsyntax.Store;
+import semant.amsyntax.Sub;
+import semant.whilesyntax.Assignment;
+import semant.whilesyntax.Minus;
+import semant.whilesyntax.Num;
+import semant.whilesyntax.Plus;
+import semant.whilesyntax.Var;
+
+import static java.util.Arrays.*;
+
+public class SimpleArithmeticCompilerTest extends CompilerTestBase {
+	
+	public SimpleArithmeticCompilerTest() {
+		ast = new Assignment(new Var("index"), new Plus(new Num("0"), new Minus(new Num("5"), new Num("3"))));
+		code = ast.accept(compiler);
+	}
+	
+	@Before
+	public void setupExpectedCode() {
+		Inst push0 = new Push("0");
+		Inst push5 = new Push("5");
+		Inst push3 = new Push("3");
+		Inst sub = new Sub();
+		Inst storeIndex = new Store("index");
+		Inst plus = new Add();
+		expected.addAll(asList(push3, push5, sub, push0, plus, storeIndex));
+		for (Inst i : expected) {
+			i.stmControlPoint = 0;
+		}
+	}
+
+}
