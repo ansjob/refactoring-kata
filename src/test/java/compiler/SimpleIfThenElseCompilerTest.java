5131d41f301778f281f839efa1d12ead7665a8a4
ansjob <ansjob@george.(none)>
2013-06-11 20:56:48 +0200

    added compilation test for ifthenelse

---
+++ src/test/java/compiler/SimpleIfThenElseCompilerTest.java
@@ -0,0 +1,57 @@
+package compiler;
+
+import org.junit.Before;
+
+import semant.amsyntax.Branch;
+import semant.amsyntax.Code;
+import semant.amsyntax.Inst;
+import semant.amsyntax.Noop;
+import semant.amsyntax.Push;
+import semant.amsyntax.Store;
+import semant.amsyntax.True;
+import semant.whilesyntax.Assignment;
+import semant.whilesyntax.Bexp;
+import semant.whilesyntax.Conditional;
+import semant.whilesyntax.Num;
+import semant.whilesyntax.Skip;
+import semant.whilesyntax.Stm;
+import semant.whilesyntax.TrueConst;
+import semant.whilesyntax.Var;
+
+import static java.util.Arrays.*;
+
+public class SimpleIfThenElseCompilerTest extends CompilerTestBase {
+	
+	Stm ifBody = new Assignment(new Var("foo"), new Num("5"));
+	Stm elseBody = new Skip();
+	Bexp b = new TrueConst();
+	
+	public SimpleIfThenElseCompilerTest() {
+		ast = new Conditional(b, ifBody, elseBody);
+		code = ast.accept(compiler);
+	}
+	
+	@Before
+	public void setup() {
+		Inst tru = new True();
+		tru.stmControlPoint = 0;
+		
+		Code ifBodyCode = new Code();
+		Inst push5 = new Push("5");
+		Inst storeFoo = new Store("foo");
+		push5.stmControlPoint = storeFoo.stmControlPoint = 1;
+		ifBodyCode.addAll(asList(push5, storeFoo));
+		
+		Code elseBodyCode = new Code();
+		Inst noop = new Noop();
+		noop.stmControlPoint = 2;
+		elseBodyCode.add(noop);
+		
+		Inst cond = new Branch(ifBodyCode, elseBodyCode);
+		cond.stmControlPoint = 0;
+		
+		expected.add(tru);
+		expected.add(cond);
+	}
+
+}
